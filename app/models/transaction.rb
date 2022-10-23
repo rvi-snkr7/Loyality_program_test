eee4915f302649567ab67328b2c1b2cdda61fea3
rvisnkr <raptor23298@gmail.com>
2022-10-23 11:43:12 +0530

    point_earn_rule level 2 Done!

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -5,7 +5,7 @@ class Transaction < ApplicationRecord
   validates :currency, presence: true
   belongs_to :user
 
-  after_create :credit_points
+  after_create :credit_points,:double_point_credit
 
   private
   def credit_points
@@ -13,4 +13,10 @@ class Transaction < ApplicationRecord
       puts "You recieve 10 points"
     end
   end
+  def double_point_credit
+    if self.currency !="INR"
+      puts "You recieve 20 standars point for making transaction in Foriegn country"
+    end
+  end
+
 end
